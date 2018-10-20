e911b795c6bcd5bdee99810da030343586f74dea
Amir Hossein Bakhtiary <amir.h.bakhtiary@gmail.com>
2018-10-20 22:56:58 +0200

    add endpoint for returning model prediction

--- model_server/serve.py
+++ model_server/serve.py
@@ -1,7 +1,8 @@
 import predict
 import numpy as np
+import urllib.request
 
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 graph, model  = predict.get_resNet50_graph_model()
 
@@ -9,10 +10,25 @@ graph, model  = predict.get_resNet50_graph_model()
 def hello():
 
     img_path = 'elephant.jpg'    
-    predict.make_inference(graph, model, img_path)
+    print(predict.make_inference(graph, model, img_path))
 
     return "Hello World!"
-     
+
+@app.route("/predict_things", methods=['GET', 'POST'])
+def predict_things():
+    
+    image = request.get_json()['image']
+    filename = image.split("/")[-1]
+    print(filename)
+    
+    urllib.request.urlretrieve(image, filename)
+    prediction = predict.make_inference(graph, model, filename)
+
+    top_choice = prediction[0][0][1]
+    top_probability = float(prediction[0][0][2])
+    print(top_choice, top_probability)
+    return jsonify(top_choice, top_probability)
+
 if __name__ == "__main__":
     app.run()
 
