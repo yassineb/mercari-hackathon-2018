342aef0c8a25835bb42fd159da06a7159a71b1a4
Amir Hossein Bakhtiary <amir.h.bakhtiary@gmail.com>
2018-10-20 21:15:06 +0200

    add model loading and prediction functions

--- model_server/serve.py
+++ model_server/serve.py
@@ -1,8 +1,16 @@
+import predict
+import numpy as np
+
 from flask import Flask
 app = Flask(__name__)
- 
+model = predict.get_resNet50_model()
+
 @app.route("/")
 def hello():
+
+    img_path = 'elephant.jpg'    
+    predict.make_inference(model, img_path)
+
     return "Hello World!"
      
 if __name__ == "__main__":
