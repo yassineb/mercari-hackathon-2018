9c0a4b2634f9a5a29c34248720fa281a037d983d
Amir Hossein Bakhtiary <amir.h.bakhtiary@gmail.com>
2018-10-20 21:28:21 +0200

    make flask work with tensorflow

--- model_server/serve.py
+++ model_server/serve.py
@@ -3,13 +3,13 @@ import numpy as np
 
 from flask import Flask
 app = Flask(__name__)
-model = predict.get_resNet50_model()
+graph, model  = predict.get_resNet50_graph_model()
 
 @app.route("/")
 def hello():
 
     img_path = 'elephant.jpg'    
-    predict.make_inference(model, img_path)
+    predict.make_inference(graph, model, img_path)
 
     return "Hello World!"
      
