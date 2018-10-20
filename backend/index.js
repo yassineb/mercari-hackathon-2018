63292811cce4d3cfecbb1bd73399787cacc89f43
Yassine Belcaid <yassine.belcaid@gmail.com>
2018-10-20 16:08:43 +0200

    Items endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
-const axios = require('axios')
 const app = express()
 const port = 3000
-require('./db.js')
+const db = require('./db.js')
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
@@ -12,15 +11,15 @@ app.use((req, res, next) => {
 })
 
 app.get('/items', async (req, res) => {
-  let productsAmount = 10;
-  let response = await axios
-    .get(`http://names.drycodes.com/${productsAmount}?nameOptions=objects`)
-  let products = response.data
-    .map(name => ({
-      name,
-      image: "https://picsum.photos/200?random&"+name
-    }));
-  res.json(products)
+  let items = await db.knex.select().from('items').map(function(item) {
+    return {
+        'id': item.id,
+        'title': item.title,
+        'image': item.image
+    }
+  });
+
+  res.json(items)
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
