ad2ee9bedabbcd0f1366220263c08c67269371fa
Ivan Nikolic <ivannkf@gmail.com>
2018-10-20 15:06:37 +0200

    Add items endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
 require('./config.js')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+  next()
+})
+
+app.get('/items', async (req, res) => {
+  let productsAmount = 10;
+  let response = await axios
+    .get(`http://names.drycodes.com/${productsAmount}?nameOptions=objects`)
+  let products = response.data
+    .map(name => ({
+      name,
+      image: "https://picsum.photos/200/300"
+    }));
+  res.json(products)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
