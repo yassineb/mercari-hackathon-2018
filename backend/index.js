ca693311070d6a0bcf50c779036847402ec77078
Yassine Belcaid <yassine.belcaid@gmail.com>
2018-10-20 15:32:02 +0200

    Rename config.js to db.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const port = 3000
-require('./config.js')
+require('./db.js')
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
